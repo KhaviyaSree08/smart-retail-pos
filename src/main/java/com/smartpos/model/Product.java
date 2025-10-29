0ab5f5a1d700d14a8d3f422695b63231a83daf47
KhaviyaSree <khaviyasreem@gmail.com>
2025-10-29 12:39:44 +0530

    updated product for product tracking

--- src/main/java/com/smartpos/model/Product.java
+++ src/main/java/com/smartpos/model/Product.java
@@ -4,33 +4,21 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "products")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String sku;        // barcode or product code
+    private String sku;
 
     @Column(nullable = false)
     private String name;
 
-    private String description;
-
-    @Column(nullable = false)
     private Double price;
-
-    @Column(nullable = false)
     private Integer stock;
-
     private Double taxPercent = 0.0;
-
-    private boolean active = true;
 }
-
