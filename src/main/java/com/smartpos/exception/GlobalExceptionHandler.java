566de8800b05851c64d322995ee5c38cad06ddc5
KhaviyaSree <khaviyasreem@gmail.com>
2025-10-29 12:50:31 +0530

    added  exception handler

---
+++ src/main/java/com/smartpos/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.smartpos.exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import java.util.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAllExceptions(Exception ex, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", new Date());
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
