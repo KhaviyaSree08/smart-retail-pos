9127768c18c3ce6e4c588c1fa398f29558b58820
KhaviyaSree <khaviyasreem@gmail.com>
2025-10-29 13:01:32 +0530

    add Swagger UI for POS API documentation

---
+++ src/main/java/com/smartpos/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.smartpos.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Smart Retail POS API")
+                        .version("1.0")
+                        .description("API documentation for POS and Billing Management System"));
+    }
+}
