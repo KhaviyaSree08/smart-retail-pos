f17f46aaed65b95ac87d3ef4cb53c63ee9e27116
KhaviyaSree <khaviyasreem@gmail.com>
2025-10-29 09:54:34 +0530

    Initial Spring Boot setup with MySQL configuration

---
+++ src/main/java/com/smartpos/SmartRetailPosApplication.java
@@ -0,0 +1,13 @@
+package com.smartpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRetailPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRetailPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpos/SmartRetailPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRetailPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
